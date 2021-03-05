2b1fd1d48b2e0bb0eb5f820e89b36414b3201150
GrumpyCello <kfoniciello@gmail.com>
2021-03-05 11:59:52 -0700

    Added the weeklyUpdateCompaniesRank Function along with documentation for how to deploy to firebase, test functions, etc.

--- README.md
+++ README.md
@@ -1,20 +1,26 @@
 # Firebase Functionalities for the IBC Wallet app
 
-## Functions
-------------
-### Auto-archive companies
-Script archiving based on company creation date run on last day of 4th, 7th, & 12th month
 
-### Update the company ranks (top1 or top4)
-Weekly Script that updates which companies are in top1 and top4
+## Getting Started
+__________________
+1. Run `npm i` from the root project folder
+2. Have java installed (https://openjdk.java.net/install/)
+3. Install Google Cloud SDK (https://cloud.google.com/sdk/docs/install)
+   1. Login to the account linked to the ibc-wallet projects
+   2. For testing, select the project 'ibc-wallet' (you can switch that later once you're ready to deploy to the production project by runing `firebase use byui-ibc-wallet`)
+4. Running/Testing Functions: `npm start`
 
-**Ranking System** \
-Weights:
-* rating = ranges from 100% to 200% of the combined pageVisitsCt and contentUpdateCt
-* pageVisitsCt = 70%
-* contentUpdateCt = 30%
+Deploy all changes: `firebase deploy`\
+Deploy specific function(s): `firebase deploy --only "functions:functionNameHere"`\
+Debug a deploy: `firebase --debug deploy`
 
-System: `rank = (rating/5+1) * (pageVisitsCt*0.7 + contentUpdateCt*0.3)`
+### Note
+To test Pub/Sub functions you will need to deploy them, and please do so to ibc-wallet (the dev one)
 
-### Write script (Firebase Function) to auto-delete 4yr old companies
-Script deleting companies 4 years old or more that runs mid-summers.
+
+[comment]: <> (4. Install the pubsub-emulator with `gcloud components install pubsub-emulator` &#40;This is to run/test scheduled functions&#41;)
+[comment]: <> (   1. Run `firebase emulators:start` for functions that do not use Pub/Sub)
+[comment]: <> (      or)
+[comment]: <> (   2. Run `gcloud beta emulators pubsub start --project=ibc-wallet` for IBC-Wallet-Dev or `byui-ibc-wallet` for IBC-Wallet-Prod)
+[comment]: <> (      1. Then run `gcloud beta emulators pubsub env-init > set_vars.cmd && set_vars.cmd`)
+[comment]: <> (      2. To stop running do `ctrl+c` or `cmd+c` then run `set PUBSUB_EMULATOR_HOST=`)
--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,50 @@
+/**
+ * Write a scheduled function:
+ * https://firebase.google.com/docs/functions/schedule-functions
+ *
+ * Create and Deploy Your First Cloud Functions
+ * https://firebase.google.com/docs/functions/write-firebase-functions
+ */
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
 
-// Take the text parameter passed to this HTTP endpoint and insert it into 
-// Firestore under the path /messages/:documentId/original
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await admin.firestore().collection('messages').add({original: original});
-  // Send back a message that we've successfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
-});
+/**
+ * Update the Companies' Rank
+ * Weekly updates which companies are in top1 and top4
+ *
+ * Ranking System
+ * Weights:
+ *   rating = ranges from 100% to 200% of the combined pageVisitsCt and contentUpdateCt
+ *   pageVisitsCt = 70%
+ *   contentUpdateCt = 30%
+ *
+ * System: rank = (rating/5+1) * (pageVisitsCt*0.7 + contentUpdateCt*0.3)
+ *
+ * If companies reaches top 4 or top 1, then they receive an email congratulating them
+ */
+exports.weeklyUpdateCompaniesRank = functions.pubsub.schedule('every monday 00:00')
+  .timeZone('America/Denver')
+  .onRun(async context => {
+    try {
+      const companiesRef = admin.firestore().collection('activeCompanies')
+      const companiesSnapshot = await companiesRef.where('contentUpdateCt', '>', 0).get()
+      if (companiesSnapshot.empty) {
+        console.info('No active companies found to update')
+        return null;
+      }
 
+      companiesSnapshot.forEach(companySnapshot => {
+        const company = companySnapshot.data()
+
+        const rating = company.rating.sum / company.rating.ratingsCt
+        companySnapshot.ref.update({
+          rank: (rating/5+1) * (company.pageVisitsCt*0.7 + company.contentUpdateCt*0.3)
+        })
+      })
+      return null;
+    } catch (error) {
+      console.error("Unable to update companies' rank: ", error)
+    }
+});
